99a4110f1ae7515ebc7941edb68a1f5cce888ff1
adminstratserver <admin@optionnalytics.com>
2019-12-27 18:12:05 +0800

    initial commit

---
+++ ansible/run_playbook_VNC.sh
@@ -0,0 +1,3 @@
+#ansible-playbook A_createnewuser_VNC.yaml -i inventry_VNC.txt -u root
+ansible-playbook B_serversecurity_VNC.yaml -i inventry_VNC.txt -u root
+#ansible-playbook C_softwareupdate_VNC.yaml -i inventry_VNC.txt -u tws002
