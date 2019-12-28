de4084938839f25a98bd84d0c12d2483ae0e9b74
adminstratserver <admin@optionnalytics.com>
2019-12-28 14:57:17 +0800

    changes made on tws

--- ansible/run_playbook_VNC.sh
+++ ansible/run_playbook_VNC.sh
@@ -1,3 +1,5 @@
-#ansible-playbook A_createnewuser_VNC.yaml -i inventry_VNC.txt -u root
+ansible-playbook A_createnewuser_VNC.yaml -i inventry_VNC.txt -u root
 ansible-playbook B_serversecurity_VNC.yaml -i inventry_VNC.txt -u root
-#ansible-playbook C_softwareupdate_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook C_gitpull_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook D_systemupdate_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook E_copyfiles_VNC_01.yaml -i inventry_VNC.txt -u tws002
