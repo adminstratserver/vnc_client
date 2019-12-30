7fe5756ec8a14b78ab065fddf62e17ac8233311c
adminstratserver <admin@optionnalytics.com>
2019-12-30 19:08:58 +0800

    changes to ansible playbook

---
+++ ansible/run_VNC.sh
@@ -0,0 +1,6 @@
+#ansible-playbook A_createnewuser_VNC.yaml -i inventry_VNC.txt -u root
+#ansible-playbook B_serversecurity_VNC.yaml -i inventry_VNC.txt -u root
+#ansible-playbook C_systemupdate_VNC.yaml -i inventry_VNC.txt -u root
+#ansible-playbook D_gitpull_VNC.yaml -i inventry_VNC.txt -u tws002
+#ansible-playbook E_setupvncserver_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook F_installtwsapp_001_VNC.yaml -i inventry_VNC.txt -u tws002
--- ansible/run_playbook_VNC.sh
+++ ansible/run_playbook_VNC.sh
@@ -1,5 +1,6 @@
 ansible-playbook A_createnewuser_VNC.yaml -i inventry_VNC.txt -u root
 ansible-playbook B_serversecurity_VNC.yaml -i inventry_VNC.txt -u root
-ansible-playbook C_gitpull_VNC.yaml -i inventry_VNC.txt -u tws002
-ansible-playbook D_systemupdate_VNC.yaml -i inventry_VNC.txt -u tws002
-ansible-playbook E_copyfiles_VNC_01.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook C_systemupdate_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook D_gitpull_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook E_setupvncserver_VNC.yaml -i inventry_VNC.txt -u tws002
+ansible-playbook F_installtwsapp_VNC.yaml -i inventry_VNC.txt -u tws002
